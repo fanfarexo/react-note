6603530d88a9c3b53aadb0f1d3db10166cbeab3c
fanfarexo <fanfarexo@gmail.com>
2024-08-14 18:17:11 +0900

    feat: (useReducer) form-obj 리듀서 적용

--- src/useReducer-components/form-obj.jsx
+++ src/useReducer-components/form-obj.jsx
@@ -1,25 +1,53 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialState = { name: 'Taylor', age: 40 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change_name': {
+      return {
+        name: action.nextName,
+        age: state.age,
+      };
+    }
+    case 'decrement_age': {
+      return {
+        name: state.name,
+        age: state.age - 1,
+      };
+    }
+    case 'increment_age': {
+      return {
+        name: state.name,
+        age: state.age + 1,
+      };
+    }
+  }
+  throw Error('Unknown action: ', action.type);
+};
 
 const FormObj = () => {
-  const [name, setName] = useState('Taylor');
-  const [age, setAge] = useState(40);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const onChange = (e) => {
-    setName(e.target.value);
+    dispatch({
+      type: 'change_name',
+      nextName: e.target.value,
+    });
   };
   const decrement = () => {
-    setAge((prevAge) => prevAge - 1);
+    dispatch({ type: 'decrement_age' });
   };
   const increment = () => {
-    setAge((prevAge) => prevAge + 1);
+    dispatch({ type: 'increment_age' });
   };
 
   return (
     <>
       <p>
-        Hello, {name}. You are {age}
+        Hello, {state.name}. You are {state.age}
       </p>
-      <input type='text' value={name} onChange={onChange} />
+      <input type='text' value={state.name} onChange={onChange} />
       <button onClick={decrement}>Decrement age</button>
       <button onClick={increment}>Increment age</button>
     </>
