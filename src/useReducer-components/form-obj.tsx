9fad8e4863a5f00f79d2a0e5bc88985e28ad18a1
fanfarexo <fanfarexo@gmail.com>
2024-08-16 13:19:35 +0900

    chore: form-obj TypeScript 마이그레이션

--- src/useReducer-components/form-obj.tsx
+++ src/useReducer-components/form-obj.tsx
@@ -1,8 +1,15 @@
-import { useReducer } from 'react';
+import { ChangeEvent, useReducer } from 'react';
+
+type initialStateType = { name: string; age: number };
+
+type actionType =
+  | { type: 'change_name'; nextName: string }
+  | { type: 'decrement_age' }
+  | { type: 'increment_age' };
 
 const initialState = { name: 'Taylor', age: 40 };
 
-const reducer = (state, action) => {
+const reducer = (state: initialStateType, action: actionType): initialStateType => {
   switch (action.type) {
     case 'change_name': {
       return {
@@ -23,12 +30,12 @@ const reducer = (state, action) => {
       };
     }
   }
-  throw Error('Unknown action: ', action.type);
 };
+
 const FormObj = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const onChange = (e) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch({
       type: 'change_name',
       nextName: e.target.value,
